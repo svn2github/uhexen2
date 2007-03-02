d806aacb2d5d6f7bd99962be7bdbd3f61ac6de28
sezero <sezero@eaf649e6-6c13-0410-b194-84221022858e>
2007-03-02 06:35:59 +0000

    tiny cleanups in autogen.sh and configure.in
    git-svn-id: svn://svn.code.sf.net/p/uhexen2/code/trunk@2282 eaf649e6-6c13-0410-b194-84221022858e

--- xdelta11/autogen.sh
+++ xdelta11/autogen.sh
@@ -6,24 +6,18 @@ DIE=0
 (autoconf --version) < /dev/null > /dev/null 2>&1 || {
 	echo
 	echo "You must have autoconf installed to compile xdelta."
-	echo "Download the appropriate package for your distribution,"
-	echo "or get the source tarball at ftp://ftp.gnu.org/pub/gnu/"
 	DIE=1
 }
 
 (libtool --version) < /dev/null > /dev/null 2>&1 || {
 	echo
 	echo "You must have libtool installed to compile xdelta."
-	echo "Get ftp://alpha.gnu.org/gnu/libtool-1.2d.tar.gz"
-	echo "(or a newer version if it is available)"
 	DIE=1
 }
 
 (automake --version) < /dev/null > /dev/null 2>&1 || {
 	echo
 	echo "You must have automake installed to compile xdelta."
-	echo "Get ftp://ftp.cygnus.com/pub/home/tromey/automake-1.4.tar.gz"
-	echo "(or a newer version if it is available)"
 	DIE=1
 }
 
@@ -36,10 +30,10 @@ fi
 	exit 1
 }
 
-if test -z "$*"; then
-	echo "I am going to run ./configure with no arguments - if you wish "
-        echo "to pass any to it, please specify them on the $0 command line."
-fi
+#if test -z "$*"; then
+#	echo "I am going to run ./configure with no arguments - if you wish "
+#	echo "to pass any to it, please specify them on the $0 command line."
+#fi
 
 for i in .
 do
