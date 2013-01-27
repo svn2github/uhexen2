svn2github/uhexen2

commit ecb2fd9112cebcd6303b8c922c0216edebe8b000

* timidity, resample.c (pre_resample): some simplification/tidy-ups. * updated version date stamp to 2013-01-25.
git-svn-id: svn://svn.code.sf.net/p/uhexen2/code/trunk@5064 eaf649e6-6c13-0410-b194-84221022858e

libs/timidity/resample.c
@@ -568,24 +568,16 @@ void pre_resample(MidSong *song, MidSample *sp)
   for(i = 0; i < count; i++)
     {
       vptr = src + (ofs >> FRACTION_BITS);
-      /*
-       * Electric Fence to the rescue: Accessing *(vptr - 1) is not a
-       * good thing to do when vptr <= src. (TiMidity++ has a similar
-       * safe-guard here.)
-       */
-      v1 = (vptr == src) ? *vptr : *(vptr - 1);
+    /*v1 = ((vptr>=src+1)? *(vptr - 1):0);*/
+      v1 = (vptr > src) ?
+	   *(vptr - 1) : *vptr /* not 0 to not distort the first sample */;
       v2 = *vptr;
       v3 = *(vptr + 1);
       v4 = *(vptr + 2);
       xdiff = FSCALENEG(ofs & FRACTION_MASK, FRACTION_BITS);
       v = (sint32)(v2 + (xdiff / 6.0) * (-2 * v1 - 3 * v2 + 6 * v3 - v4 +
        xdiff * (3 * (v1 - 2 * v2 + v3) + xdiff * (-v1 + 3 * (v2 - v3) + v4))));
-      if(v < -32768)
-	  *dest++ = -32768;
-      else if(v > 32767)
-	  *dest++ = 32767;
-      else
-	  *dest++ = (sint16)v;
+      *dest++ = (sint16)((v1 > 32767) ? 32767 : ((v1 < -32768) ? -32768 : v1));
       ofs += incr;
     }
 

docs/ReleaseNotes
@@ -1,7 +1,7 @@
 Hammer of Thyrion (uHexen2) release notes:
 
 ========================================================================
-Changes in v1.5.6: (Jan. 23, 2013.)
+Changes in v1.5.6: (Jan. 25, 2013.)
 ========================================================================
 
 - OpenGL: worked around weird mods, e.g.: project peanut, where certain

engine/hexen2/quakedef.h
@@ -30,7 +30,7 @@
 #define	HOT_VERSION_MAJ		1
 #define	HOT_VERSION_MID		5
 #define	HOT_VERSION_MIN		6
-#define	HOT_VERSION_REL_DATE	"2013-01-23"
+#define	HOT_VERSION_REL_DATE	"2013-01-25"
 #define	HOT_VERSION_STR		STRINGIFY(HOT_VERSION_MAJ) "." STRINGIFY(HOT_VERSION_MID) "." STRINGIFY(HOT_VERSION_MIN)
 #define	GLQUAKE_VERSION		1.00
 #define	ENGINE_VERSION		1.27

engine/hexenworld/shared/quakedef.h
@@ -30,7 +30,7 @@
 #define	HOT_VERSION_MAJ		1
 #define	HOT_VERSION_MID		5
 #define	HOT_VERSION_MIN		6
-#define	HOT_VERSION_REL_DATE	"2013-01-23"
+#define	HOT_VERSION_REL_DATE	"2013-01-25"
 #define	HOT_VERSION_STR		STRINGIFY(HOT_VERSION_MAJ) "." STRINGIFY(HOT_VERSION_MID) "." STRINGIFY(HOT_VERSION_MIN)
 #define	GLQUAKE_VERSION		1.00
 #define	ENGINE_VERSION		0.27

commit 04de9817f47b96f971dc3c4b743a7d693d5f38c1

timidity, resample.c (pre_resample): fixed a typo introduced by       commit r5064.
git-svn-id: svn://svn.code.sf.net/p/uhexen2/code/trunk@5072 eaf649e6-6c13-0410-b194-84221022858e

libs/timidity/resample.c
@@ -584,7 +584,7 @@ void pre_resample(MidSong *song, MidSample *sp)
       xdiff = FSCALENEG(ofs & FRACTION_MASK, FRACTION_BITS);
       v = (sint32)(v2 + (xdiff / 6.0) * (-2 * v1 - 3 * v2 + 6 * v3 - v4 +
        xdiff * (3 * (v1 - 2 * v2 + v3) + xdiff * (-v1 + 3 * (v2 - v3) + v4))));
-      *dest++ = (sint16)((v1 > 32767) ? 32767 : ((v1 < -32768) ? -32768 : v1));
+      *dest++ = (sint16)((v > 32767) ? 32767 : ((v < -32768) ? -32768 : v));
       ofs += incr;
     }
 