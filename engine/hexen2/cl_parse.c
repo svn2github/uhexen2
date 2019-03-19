a3b2a2bc88e2a6d8e2b88391ab44b21fd51b9047
Shanjaq <shanjaq@gmail.com>
2019-03-19 01:02:33 -0700

    clients parse gamedir for p21

--- engine/hexen2/cl_parse.c
+++ engine/hexen2/cl_parse.c
@@ -270,34 +270,37 @@ static void CL_ParseServerInfo (void)
 	cl.scores = (scoreboard_t *) Hunk_AllocName (cl.maxclients*sizeof(*cl.scores), "scores");
 
 // parse gamedir
-	str = MSG_ReadString();
-
-	if (q_strcasecmp(fs_gamedir_list, str))
+	if (cl_protocol == PROTOCOL_UH2_114)
 	{
-		Con_Printf("Server set the gamedir to %s\n", str);
-		// save current config
-		//Host_WriteConfiguration("config.cfg");
+		str = MSG_ReadString();
 
-		// set the new gamedirs and userdir
-		FS_Gamedir(str);
-
-		// ZOID - run autoexec.cfg in the gamedir if it exists
-		if (FS_FileInGamedir("config.cfg"))
+		if (q_strcasecmp(fs_gamedir_list, str))
 		{
-			// remove any weird mod specific key bindings / aliases
-			Cbuf_AddText("unbindall\n");
-			Cbuf_AddText("unaliasall\n");
-			Cbuf_AddText("exec autoexec.cfg\n");
-			Cbuf_AddText("exec config.cfg\n");
+			Con_Printf("Server set the gamedir to %s\n", str);
+			// save current config
+			//Host_WriteConfiguration("config.cfg");
+
+			// set the new gamedirs and userdir
+			FS_Gamedir(str);
+
+			// ZOID - run autoexec.cfg in the gamedir if it exists
+			if (FS_FileInGamedir("config.cfg"))
+			{
+				// remove any weird mod specific key bindings / aliases
+				Cbuf_AddText("unbindall\n");
+				Cbuf_AddText("unaliasall\n");
+				Cbuf_AddText("exec autoexec.cfg\n");
+				Cbuf_AddText("exec config.cfg\n");
+			}
+			// gamespy crap
+			if (FS_FileInGamedir("frontend.cfg"))
+				Cbuf_AddText("exec frontend.cfg\n");
+
+			Cbuf_Execute();
+
+			// re-init draw
+			Draw_ReInit();
 		}
-		// gamespy crap
-		if (FS_FileInGamedir("frontend.cfg"))
-			Cbuf_AddText("exec frontend.cfg\n");
-
-		Cbuf_Execute();
-
-		// re-init draw
-		Draw_ReInit();
 	}
 
 // parse gametype
