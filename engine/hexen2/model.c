7cb8658ac5137ca47194039890439377aad6dc7d
Shanjaq <shanjaq@gmail.com>
2020-12-26 19:46:00 -0800

    fix FS_LoadStackFile too few args

--- engine/hexen2/model.c
+++ engine/hexen2/model.c
@@ -290,7 +290,7 @@ static qmodel_t *Mod_LoadModel (qmodel_t *mod, qboolean crash)
 //
 // load the file
 //
-	buf = FS_LoadStackFile (mod->name, stackbuf, sizeof(stackbuf), & mod->path_id);
+	buf = FS_LoadStackFile (mod->name, stackbuf, sizeof(stackbuf), & mod->path_id, NULL);
 	if (!buf)
 	{
 		if (crash)
